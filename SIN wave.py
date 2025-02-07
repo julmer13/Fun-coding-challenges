fe1973b308db34dc64a8b4e95ad7145822851233
julmer13 <jtu@ulmerfamily.net>
2025-02-07 18:00:50 +0000

    fixed the len of the list input

--- SIN wave.py
+++ SIN wave.py
@@ -15,8 +15,8 @@ def first_wave(number_list, len_of_list):
                         new_list.append(number)
     return new_list
 
-def next_wave(number_list):
-    new_number = number_list[len(number_list)-1]
+def next_wave(number_list, new_number, first_list):
+    new_number = first_list[new_number]
     new_list = [0]
     for i in range(0, len(number_list) - 1, 1):
         new_list.append(number_list[i])
@@ -26,6 +26,7 @@ def next_wave(number_list):
 #clear the screen and welcome the user
 os.system('cls' if os.name == 'nt' else 'clear')
 input_number = int(input(f"Give me the top/bottom value for a wave of numbers: "))
+list_size = int(input(f"What size do you want the list to be: "))
 
 list_of_possible_numbers = []
 
@@ -34,11 +35,15 @@ for i in range(input_number, -1, -1):
 for i in range(1, input_number):
     list_of_possible_numbers.append(input_number - i)
 
-fred = first_wave(list_of_possible_numbers, len(list_of_possible_numbers * 2))
-print(first_wave(list_of_possible_numbers, len(list_of_possible_numbers * 2)))
+numbers_to_use = first_wave(list_of_possible_numbers, len(list_of_possible_numbers) * 2)
+
+starting_list = first_wave(list_of_possible_numbers, list_size)
+print(first_wave(list_of_possible_numbers, list_size))
+changing_list = starting_list
 
 while True:
-    print(next_wave(fred))
-    time.sleep(0.5)
-    fred = next_wave(fred)
+    for i in range(-1, (len(numbers_to_use) * -1) - 1, -1):
+        print(next_wave(changing_list, i, numbers_to_use))
+        time.sleep(0.5)
+        changing_list = next_wave(changing_list, i, numbers_to_use)
     
