6d63c25780e9d4a540224a06b98ade66e7a75faf
julmer13 <jtu@ulmerfamily.net>
2025-02-11 14:36:32 +0000

    added game over triggers and made grd nice

--- Snake Game.py
+++ Snake Game.py
@@ -30,26 +30,37 @@ def get_key(timeout):
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setraw(fd)
-        # Use select to check if there's input available
         rlist, _, _ = select.select([sys.stdin], [], [], timeout)
         if rlist:
-            key = sys.stdin.read(3)  # Read 3 bytes (for arrow keys)
+            key = sys.stdin.read(3) 
         else:
             key = None
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return key
 
-move = "up"
-grid = [[" " for _ in range(20)] for _ in range(20)]
-speed = float(input(f"what do you want the speed to be"))
-grid[5][4] = "#"
-current_places = [4, 5]
-for row in grid:
-    #print the grid
-    print(row)
+move = "non"
+os.system('cls' if os.name == 'nt' else 'clear')
+speed = float(input(f"what do you want the speed to be: "))
+length = int(input(f"what do you want the length to be: "))
+size = int(input(f"what do you want the size to be: "))
+grid = [[" " for _ in range(size)] for _ in range(size)]
+game_state = "wining"
+grid[(size // 2) - 1][(size // 2) - 1] = "#"
+current_places = [(size // 2) - 1, (size // 2) - 1]
+for y in range(size):
+    grid[y][0] = "|"
+    grid[y][size -1] = "|"
+    grid[0][y] = "-"
+    grid[size - 1][y] = "-"
+    grid[size - 1][0] = "-"
 
-while True:
+while game_state == "wining":
+    if len(current_places) // 2 > length - 1:
+        tail_x = current_places.pop(0)
+        tail_y = current_places.pop(0)
+        grid[tail_y][tail_x] = " "
+
     start_time = time.perf_counter()
     key = get_key(speed)
     if key == '\x1b[A' and move != "down":
@@ -64,21 +75,35 @@ while True:
     elif key == '\x1b[C' and move != "left":
         move_snake(current_places[-2], current_places[-1], "right", grid, current_places)
         move = "right"
-    else:
+    elif move != "non":
         move_snake(current_places[-2], current_places[-1], move, grid, current_places)
 
     end_time = time.perf_counter()
     if end_time - start_time < speed:
         time.sleep(speed - (end_time - start_time))
     
-    if len(current_places) // 2 > 4:
-        tail_x = current_places.pop(0)
-        tail_y = current_places.pop(0)
-        grid[tail_y][tail_x] = " "
-
     os.system('cls' if os.name == 'nt' else 'clear')
 
     for row in grid:
         #print the grid
-        print(row)
-    print( )
+        print(" ".join(row))
+
+    if current_places[-1] == 0 or current_places[-2] == 0 or current_places[-1] == size - 1 or current_places[-2] == size - 1:
+        game_state = "loss"
+    
+    points_seen = set()
+    for i in range(0, len(current_places), 2):
+        point = (current_places[i], current_places[i + 1])
+        if point in points_seen:
+            grid[current_places[i + 1]][current_places[i]] = "X"
+            game_state = "loss"
+            break
+        points_seen.add(point)
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+for row in grid:
+    #print the grid
+    print(" ".join(row))
+
+print("Game over")
