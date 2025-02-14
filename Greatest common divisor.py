5382308403af3b9a44b55093a1b937165d3e2536
julmer13 <jtu@ulmerfamily.net>
2025-02-14 01:31:23 +0000

    started work on GCD finder

---
+++ Greatest common divisor.py
@@ -0,0 +1,26 @@
+#import things
+import math
+
+#def things
+def find_GCD(a, b):
+    first_number = int(a)
+    second_number = int(b)
+    first_list = []
+    second_list = []
+    for i in range(1, math.ceil(math.sqrt(first_number))):
+        if first_number % i == 0:
+            first_list.append(i)
+            first_list.append(first_number / i)
+    print(first_list)
+    for i in range(1, math.ceil(math.sqrt(second_number))):
+        if second_number % i == 0:
+            second_list.append(i)
+            second_list.append(first_number / i)
+    print(second_list)
+    for number in range(len(first_list) - 1, 0, -1):
+        for ints in range(len(second_list) - 1, 0, -1):
+            if number == ints:
+                return number
+
+print(find_GCD(5, 20))
+
