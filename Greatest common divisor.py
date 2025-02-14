82e48c8b3a5ae456dbd12c9afb81605ba503906c
julmer13 <jtu@ulmerfamily.net>
2025-02-14 15:58:58 +0000

    fixed GCD

--- Greatest common divisor.py
+++ Greatest common divisor.py
@@ -7,15 +7,15 @@ def find_GCD(a, b):
     second_number = int(b)
     first_list = []
     second_list = []
-    for i in range(1, math.ceil(math.sqrt(first_number))):
+    for i in range(1, math.ceil(math.sqrt(first_number)) + 1):
         if first_number % i == 0:
             first_list.append(i)
             first_list.append(first_number / i)
     print(first_list)
-    for i in range(1, math.ceil(math.sqrt(second_number))):
+    for i in range(1, math.ceil(math.sqrt(second_number)) + 1):
         if second_number % i == 0:
             second_list.append(i)
-            second_list.append(first_number / i)
+            second_list.append(second_number / i)
     print(second_list)
     for number in range(len(first_list) - 1, 0, -1):
         for ints in range(len(second_list) - 1, 0, -1):
