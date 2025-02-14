8c06dbde178c67d7316a38efc022c0b8c9f6b6d5
julmer13 <jtu@ulmerfamily.net>
2025-02-14 16:10:53 +0000

    finished GCD

--- Greatest common divisor.py
+++ Greatest common divisor.py
@@ -11,16 +11,15 @@ def find_GCD(a, b):
         if first_number % i == 0:
             first_list.append(i)
             first_list.append(first_number / i)
-    print(first_list)
     for i in range(1, math.ceil(math.sqrt(second_number)) + 1):
         if second_number % i == 0:
             second_list.append(i)
             second_list.append(second_number / i)
-    print(second_list)
     for number in range(len(first_list) - 1, 0, -1):
         for ints in range(len(second_list) - 1, 0, -1):
-            if number == ints:
-                return number
+            if first_list[number] == second_list[ints]:
+                return first_list[number]
 
-print(find_GCD(5, 20))
+two_numbers = input(f"Give me to numbers and I will tell you there greatest common divisor: ").split()
+print(f"The greatest common divisor of {two_numbers[0]} and {two_numbers[1]} is: {int(find_GCD(two_numbers[0], two_numbers[1]))}")
 
