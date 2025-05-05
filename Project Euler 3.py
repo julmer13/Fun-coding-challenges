bfcc596ce95a0e64c92189637046b07fd1c01a01
julmer13 <jtu@ulmerfamily.net>
2025-05-05 16:35:36 -0400

    finished project euler 3

--- Project Euler 3.py
+++ Project Euler 3.py
@@ -4,23 +4,28 @@ import math
 
 #def things
 def is_prime(n):
-    if n <= 1:
+    if n <= 1 or n % 2 == 0 or n % 3 == 0:
         return False
-    for i in range(2, math.ceil(math.sqrt(n)) + 1):
-        if n % i == 0:
+    if n <= 3:
+        return True
+    for i in range(5, math.ceil(math.sqrt(n)) + 1, 6):
+        if n % i == 0 or n % (i + 2) == 0:
             return False
     return True
 
 def Largest_Prime_Factor(number):
-    if is_prime(number) != True:
-        prime_factor = 0
-        for i in range(math.floor(number / 2), 1, -1):
-            if number % i == 0 and is_prime(i):
-                prime_factor = i
-                break
-        if prime_factor > 0: return prime_factor
-        else: return "there is no prime factors"
-    else: return number
+    largest_number = 0
+    if is_prime(number):
+        return number
+    else:
+        for i in range(math.ceil(math.sqrt(number)) + 1, 2, -1):
+            if number % i == 0: 
+                if is_prime(i) and i > largest_number:
+                    largest_number = i
+                elif number % (number / i) and i > largest_number:
+                    largest_number = i
+
+    return largest_number
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
