ce322cf92384df7bf6205b503ba37ade5a77022d
julmer13 <jtu@ulmerfamily.net>
2025-05-07 13:14:24 +0000

    made project euler 16

---
+++ Project Euler 16.py
@@ -0,0 +1,13 @@
+#import things
+import os
+
+#def things
+def sum_of_digits(number):
+    number_of_digits = 0
+    new_number = number
+    while new_number >= 1:
+        number_of_digits += new_number % 10
+        new_number //= 10
+    print(f"The sum of the digits in {number} is {number_of_digits}")
+
+sum_of_digits(int(input(f"Give the number: ")))
