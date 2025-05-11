50ddac8a3ba9fb1c8e783c03c96f32b6c7c836a2
julmer13 <jtu@ulmerfamily.net>
2025-05-11 17:19:32 -0400

    added a clear code

--- Project Euler 16.py
+++ Project Euler 16.py
@@ -8,6 +8,7 @@ def sum_of_digits(number):
     while new_number >= 1:
         number_of_digits += new_number % 10
         new_number //= 10
+    os.system('cls' if os.name == 'nt' else 'clear')
     print(f"The sum of the digits in {number} is {number_of_digits}")
-
+os.system('cls' if os.name == 'nt' else 'clear')
 sum_of_digits(int(input(f"Give the number: ")))
