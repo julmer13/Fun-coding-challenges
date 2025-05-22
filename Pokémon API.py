34dc98364cfdd34fc470519affd188423321c114
julmer13 <jtu@ulmerfamily.net>
2025-05-22 13:01:07 +0000

    started battle card

--- Pokémon API.py
+++ Pokémon API.py
@@ -24,6 +24,39 @@ def color_text(color, text):
     color_code = color_map.get(color.lower(), Fore.RESET)
     return(f"{color_code}{text}{Style.RESET_ALL}")
 
+def make_battle_card(pokemon):
+    battle_card = [
+        [" ", "N", "a", "m", "e", ":", " "],
+        [" ", "T", "y", "p", "e", "s", ":", " "],
+        [" ", "H", "P", ":", " ", " ", "A", "t", "t", "a", "c", "k", ":", " "],
+        [" ", "H", "e", "i", "g", "h", "t", ":", " ", " ", "W", "e", "i", "g", "h", "t", ":" " "],
+    ]
+    for i in range(len(str(pokemon["name"]))):
+        battle_card[0].append(color_text(pokemon["color"], str(pokemon["name"])[i]))
+
+    for l in range(len(pokemon["types"])):
+        for i in range(len(str(pokemon["types"][l]))):
+            battle_card[1].append(str(pokemon["types"][l])[i])
+        if l < len(pokemon["types"]) - 1:
+            battle_card[1].append(",")
+            battle_card[1].append(" ")
+
+    for i in range(len(str(pokemon["hp"]))):
+        battle_card[2].insert(5 + i, str(pokemon["hp"])[i])
+
+    for i in range(len(str(pokemon["attack"]))):
+        battle_card[2].append(str(pokemon["attack"])[i])
+
+    for i in range(len(str(pokemon["height"]))):
+        battle_card[3].insert(9 + i,str(pokemon["height"])[i])
+
+    for i in range(len(str(pokemon["weight"]))):
+        battle_card[3].append(str(pokemon["weight"])[i])
+
+    for stat in battle_card:
+        print("".join(stat))
+
 for pokemon in pokemon_data:
-    if pokemon["name"] == "pikachu":
-        print(color_text(pokemon["color"], pokemon["name"]))
+    if pokemon["weight"] == 69:
+        make_battle_card(pokemon)
+        print("\n")
